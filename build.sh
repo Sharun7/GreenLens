fa6df5e317f3a33c83dc004125300b960be8d2a7
Sharun Tomy <sharuntomy5@gmail.com>
2026-05-13 17:11:13 +0530

    Fix Python version and build dependencies for Render

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
+pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py train_mlp_model
