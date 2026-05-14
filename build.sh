c5c84082ababfd526ccb0ec774b02a64d10490c9
Sharun Tomy <sharuntomy5@gmail.com>
2026-05-14 10:35:17 +0530

    Auto load CSV data during Render deploy

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ set -o errexit
 
 pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py load_cbi_bonds --file=data/green_bonds-21.csv || true
+python manage.py collectstatic --no-input
